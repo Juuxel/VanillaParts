d48b5c57b272f282aa9afef43e697e39599c083e
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-12-19 00:16:14 +0200

    Check this == CAKE in cake block's NativeMultipart

--- src/main/java/juuxel/vanillaparts/mixin/CakeBlockMixin.java
+++ src/main/java/juuxel/vanillaparts/mixin/CakeBlockMixin.java
@@ -5,6 +5,7 @@ import alexiil.mc.lib.multipart.api.NativeMultipart;
 import juuxel.vanillaparts.part.CakePart;
 import juuxel.vanillaparts.part.VPartDefinitions;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.CakeBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -19,6 +20,10 @@ abstract class CakeBlockMixin implements NativeMultipart {
     @Nullable
     @Override
     public List<MultipartContainer.MultipartCreator> getMultipartConversion(World world, BlockPos blockPos, BlockState blockState) {
-        return Collections.singletonList(holder -> new CakePart(VPartDefinitions.CAKE, holder, blockState.get(CakeBlock.BITES)));
+        if ((Object) this == Blocks.CAKE) {
+            return Collections.singletonList(holder -> new CakePart(VPartDefinitions.CAKE, holder, blockState.get(CakeBlock.BITES)));
+        }
+
+        return null;
     }
 }
